d5a6ee78153bc8c637cb8e9e93bc549e6cc2636d
moonjs0113 <littlemoom@naver.com>
2024-08-09 21:45:45 +0900

    Create Project

---
+++ XcodeTips/XcodeTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTipsApp.swift
+//  XcodeTips
+//
+//  Created by Moon Jongseek on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
