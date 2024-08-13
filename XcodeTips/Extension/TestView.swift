71107e109c2b2bbfcca9ea24f763fa11b72416a4
sanghyeon <ppppp5001@naver.com>
2024-08-13 20:03:19 +0900

    어제 이미 해놔서 pull하느라 많이 따라가진 못했습니다..

---
+++ XcodeTips/Extension/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  XcodeTips
+//
+//  Created by 박상현 on 8/13/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
